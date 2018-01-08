51f014fed447ed14352f0e987c85d7a79865c91e
Oleg Kozlov (xxblx) <xxblx@posteo.org>
2018-01-08 15:04:55 +0300

    TokenBaseHandler, TokenRefreshHandler

--- webapp/handlers.py
+++ webapp/handlers.py
@@ -52,7 +52,45 @@ class SignupHandler(BaseHandler):
         self.set_status(200)
 
 
-class TokenGetHandler(BaseHandler):
+class TokenBaseHandler(BaseHandler):
+
+    @tornado.gen.coroutine
+    def generate_token(self, username):
+        """ Generate access token and refresh token """
+
+        # Token split in two parts: selector and verifier
+        # Selector stores as is in db, but verifier stores as hash
+        selector = uuid4().hex
+        verifier = uuid4().hex
+        verifier_hash = nacl.hash.blake2b(verifier, key=self.hmac_key,
+                                          encoder=nacl.encoding.HexEncoder)
+        expires_in = datetime.now() + timedelta(hours=2)
+        expires_in = mktime(expires_in.utctimetuple())
+
+        tokens_dct = {'selector': selector, 'verifier': verifier}
+        access_token = base64.encodebytes(json.dumps(tokens_dct).encode())
+        refresh_token = uuid4().hex
+
+        # Store token in db
+        tokens_dct_db = {
+            'selector': selector,
+            'verifier': verifier_hash,
+            'refresh': refresh_token,
+            'expires_in': expires_in,
+        }
+        yield self.db.users.update({'username': username},
+                                   {'$push': {'access_tokens': tokens_dct_db}})
+
+        user_tokens = {
+            'access_token': access_token,
+            'refresh_token': refresh_token,
+            'expires_in': expires_in
+        }
+
+        return user_tokens
+
+
+class TokenGetHandler(TokenBaseHandler):
 
     @tornado.gen.coroutine
     def post(self):
@@ -74,38 +112,33 @@ class TokenGetHandler(BaseHandler):
             self.set_status(403)
             self.finish()
 
-        # Token split in two parts: selector and verifier
-        # Selector stores as is in db, but verifier stores as hash
-        selector = uuid4().hex
-        verifier = uuid4().hex
-        verifier_hash = nacl.hash.blake2b(verifier, key=self.hmac_key,
-                                          encoder=nacl.encoding.HexEncoder)
-        expires_in = datetime.now() + timedelta(hours=2)
-        expires_in = mktime(expires_in.utctimetuple())
-
-        token_dct = {'selector': selector, 'verifier': verifier}
-        access_token = base64.encodebytes(json.dumps(token_dct).encode())
-        refresh_token = uuid4().hex
-
-        # Store token in db
-        token_dct_db = {
-            'selector': selector,
-            'verifier': verifier_hash,
-            'refresh': refresh_token,
-            'expires_in': expires_in,
-        }
-        yield self.db.users.update({'username': username},
-                                   {'$push': {'access_tokens': token_dct_db}})
-
-        self.write({
-            'access_token': access_token,
-            'refresh_token': refresh_token,
-            'expires_in': expires_in
-        })
+        user_tokens = self.generate_token(username)
+        self.write(**user_tokens)
 
 
-class TokenRefreshHandler(BaseHandler):
+class TokenRefreshHandler(TokenBaseHandler):
 
     @tornado.gen.coroutine
     def post(self):
-        pass
+        access_token = self.get_argument('access_token')
+        refresh_token = self.get_argument('refresh_token')
+
+        tokens_dct = json.loads(base64.decodebytes(access_token).decode())
+        verifier_hash = nacl.hash.blake2b(tokens_dct['verifier'],
+                                          key=self.hmac_key,
+                                          encoder=nacl.encoding.HexEncoder)
+
+        # Looking for tokens in db
+        query = {
+            'access_tokens.refresh': {'$eq': refresh_token},
+            'access_tokens.selector': {'$eq': tokens_dct['selector']},
+            'access_tokens.verifier': {'$eq': verifier_hash},
+        }
+        user_dct = self.db.users.find_one(query)
+
+        if user_dct is None:
+            self.set_status(403)
+            self.finish()
+
+        user_tokens = self.generate_token(user_dct['username'])
+        self.write(**user_tokens)
